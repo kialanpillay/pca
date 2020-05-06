ccb7005684e3ccdb187602f3bd4428a16b47df51
Kialan Pillay <kialanpillay@gmail.com>
2020-05-06 15:06:34 +0200

    Initial Commit

---
+++ pca.cpp
@@ -0,0 +1,6 @@
+#include "pca.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace PLLKIA010;
---
+++ pca.h
@@ -0,0 +1,16 @@
+#ifndef PCA_H
+#define PCA_H
+
+namespace PLLKIA010
+{
+    class PCA
+    {   
+        public: 
+            PCA(void);
+            PCA();
+            ~PCA(void);
+        private: 
+    };
+}
+
+#endif
