b4282c80208650ee8cf78730deea12bceadf94d7
Kialan Pillay <kialanpillay@gmail.com>
2020-05-17 15:04:20 +0200

    Text Formatting

--- pca.cpp
+++ pca.cpp
@@ -44,18 +44,18 @@ void PCA::compute(std::ostream& os){
     cov = data.transpose() * data / (M - 1);
     //Eigenvalues and Eigenvectors
     EigenSolver<MatrixXd> es(cov);
-    os << "Question 1)\nThe Eigenvalues for the principal components are: " << std::endl << es.eigenvalues().real() << std::endl << std::endl;
-    os << "Question 2)\nThe Eigenvectors for the principal components are:" << std::endl << es.eigenvectors().real() << std::endl << std::endl;
-    os << "Col 1 - Principal Component 1 (Eigenvalue = " << es.eigenvalues()[0].real() << "), Col 2 - Principal Component 2 (Eigenvalue = " << es.eigenvalues()[1].real() << ")" << std::endl;
+    os << "Question 1\nThe Eigenvalues: " << std::endl << es.eigenvalues().real() << std::endl << std::endl;
+    os << "Question 2\nEigenvectors:" << std::endl << es.eigenvectors().real() << std::endl << std::endl;
+    os << "Col 1 - PC 1 (Eigenvalue = " << es.eigenvalues()[0].real() << "), Col 2 - PC 2 (Eigenvalue = " << es.eigenvalues()[1].real() << ")" << std::endl;
     os << "Row 1 - January (X), Row 2 - July (Y)" << std::endl << std::endl;
-    os << "Question 3)\nThe Covariance Matrix is: " << std::endl << cov << std::endl << std::endl;
+    os << "Question 3\nCovariance Matrix: " << std::endl << cov << std::endl << std::endl;
 
     //Total Variance
     double totalVariance = cov.coeff(0,0) + cov.coeff(1,1);
-    os << "Question 4)\nThe total variance, given by Var(X) + Var(Y), is: " << totalVariance << std::endl << std::endl;
+    os << "Question 4\nTotal Variance: " << totalVariance << std::endl << std::endl;
 
     //Proportional PC Variance
-    os << "Question 5)\nThe variance of each principal component is equal to its eigenvalue.\nThe proportion of total variance explained by each principal component is:" << std::endl << std::endl;
+    os << "Question 5\nPC Variance = Eigenvalue.\nProportional PC Variance" << std::endl << std::endl;
     os << "Principal Component 1: " << es.eigenvalues()[0].real()/totalVariance * 100 << "%" << std::endl;
     os << "Principal Component 2: " << es.eigenvalues()[1].real()/totalVariance * 100 << "%" << std::endl << std::endl;
 }
